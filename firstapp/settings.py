6771ce9c09c9e9ab5b1abef3b718237fb1dc35e8
Arjunkothandan <arjun.k.2024.aiml@rajalakshmi.edu.in>
2026-03-17 00:00:41 +0530

    Updated settings.py to serve uploaded images from static folder

--- firstapp/settings.py
+++ firstapp/settings.py
@@ -124,11 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = BASE_DIR/'staticfiles'
 
 #If you have a project-level static directory
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+MEDIA_URL = '/images/'         # same as STATIC_URL
+MEDIA_ROOT = BASE_DIR / 'static'
